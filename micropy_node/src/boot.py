c965fc7d082e2e62fa21f0045ec9255932b41ef2
sburklund <scott.burklund@gmail.com>
2019-12-29 17:40:33 -0600

    testmqtt connection from esp8266 board

--- micropy_node/src/boot.py
+++ micropy_node/src/boot.py
@@ -1,1 +1,26 @@
-# boot.py - - runs on boot-up
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+#set up wi-fi connection on boot. WiFi credential in config.py
+def do_connect():
+    import network
+    import config
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(config.WIFI_SSID, config.WIFI_PWD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+
+do_connect()
--- micropy_node/src/main.py
+++ micropy_node/src/main.py
@@ -1,1 +1,13 @@
 # main.py
+import os
+import config
+import machine
+import ubinascii
+from umqtt.simple import MQTTClient
+
+print(os.listdir())
+
+client_id = ubinascii.hexlify(machine.unique_id())
+client = MQTTClient(client_id, config.MQTT_HOST)
+client.connect()
+client.publish(b'/hello', b'Hello from espp ' + client_id)

07379390a063b03e7fff2cf63159f98efe28ef54
sburklund <scott.burklund@gmail.com>
2019-12-29 19:07:24 -0600

    example publish from task

--- micropy_node/src/main.py
+++ micropy_node/src/main.py
@@ -2,12 +2,32 @@
 import os
 import config
 import machine
+import uasyncio
+import uasyncio.queues
 import ubinascii
 from umqtt.simple import MQTTClient
 
 print(os.listdir())
 
-client_id = ubinascii.hexlify(machine.unique_id())
-client = MQTTClient(client_id, config.MQTT_HOST)
-client.connect()
-client.publish(b'/hello', b'Hello from espp ' + client_id)
+mqtt_queue = uasyncio.queues.Queue()
+
+async def mqtt_task(msg_queue):
+    client_id = ubinascii.hexlify(machine.unique_id())
+    client = MQTTClient(client_id, config.MQTT_HOST)
+    client.connect()
+    while True:
+        result = await msg_queue.get()
+        client.publish(result[0], result[1])
+
+async def killer():
+    await uasyncio.sleep(10)
+
+async def test_publish(msg_queue):
+    while True:
+        await uasyncio.sleep(1)
+        await msg_queue.put((b'/hello', b'Hello from async'))
+
+loop = uasyncio.get_event_loop()
+loop.create_task(mqtt_task(mqtt_queue))
+loop.create_task(test_publish(mqtt_queue))
+loop.run_until_complete(killer())

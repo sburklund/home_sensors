3effb3af5e33a3b8f133face8f86e08ab8f60d95
sburklund <scott.burklund@gmail.com>
2019-12-31 17:20:21 -0600

    add motion publishing

--- micropy_node/src/main.py
+++ micropy_node/src/main.py
@@ -7,6 +7,7 @@ import uasyncio
 import uasyncio.queues
 import ubinascii
 from umqtt.simple import MQTTClient
+import utime
 
 print(os.listdir())
 
@@ -21,7 +22,7 @@ async def mqtt_task(msg_queue):
         client.publish(result[0], result[1])
 
 async def killer():
-    await uasyncio.sleep(30)
+    await uasyncio.sleep(60)
 
 async def heartbeat_task(msg_queue):
     while True:
@@ -45,9 +46,24 @@ async def temp_humid_task(msg_queue):
         await msg_queue.put((config.MQTT_TOPIC_TEMP, str(temp_value).encode('ascii')))
         await msg_queue.put((config.MQTT_TOPIC_HUMID, str(humid_value).encode('ascii')))
 
+async def motion_task(msg_queue):
+    motion_sensor = machine.Pin(config.MOTION_SENSOR_PIN, machine.Pin.IN)
+    last_motion_time = utime.ticks_ms()
+    while True:
+        await uasyncio.sleep(config.MOTION_POLLING_PERIOD)
+        motion_value = motion_sensor.value()
+        if motion_value:
+            last_motion_time = utime.ticks_ms()
+            await msg_queue.put((config.MQTT_TOPIC_MOTION, b'Activity'))
+        elif not motion_value and utime.ticks_diff(utime.ticks_ms(), utime.ticks_add(last_motion_time, config.MOTION_MIN_ONTIME * 1000)) < 0:
+            await msg_queue.put((config.MQTT_TOPIC_MOTION, b'Activity'))
+        else:
+            await msg_queue.put((config.MQTT_TOPIC_MOTION, b'No Activity'))
+
 loop = uasyncio.get_event_loop()
 loop.create_task(mqtt_task(mqtt_queue))
 loop.create_task(heartbeat_task(mqtt_queue))
 loop.create_task(light_task(mqtt_queue))
 loop.create_task(temp_humid_task(mqtt_queue))
+loop.create_task(motion_task(mqtt_queue))
 loop.run_until_complete(killer())
